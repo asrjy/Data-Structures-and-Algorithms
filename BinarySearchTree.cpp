14336b588910901e4ae21196cc759bfdc7c6f46c
Yashwanth Allakky <yashwanth.allakky@codingstudio.club>
2019-10-14 22:39:56 +0530

    Add files via upload

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+  int data;
+  Node *left, *right;
+  Node(int x){
+    data = x;
+    left = NULL;
+    right = NULL;
+  }
+}
+int main(){
+  
+}
+
